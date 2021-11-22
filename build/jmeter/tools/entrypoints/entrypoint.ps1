bffac54461c123c24876004f02d6a599084e991e
Kevin Mazzoni <kevin.mazzoni@velir.com>
2021-11-22 14:44:49 -0500

    Adding function to entrypoint script to make sure args are cleaned for jmeter app

--- build/jmeter/tools/entrypoints/entrypoint.ps1
+++ build/jmeter/tools/entrypoints/entrypoint.ps1
@@ -1,8 +1,28 @@
+function QuoteArgument {
+    param(
+        [string]$arg
+    )
+
+    if($arg.Contains("=") -and $arg.Contains(".") -and -not $arg.Contains('"')) {
+        $parts = $arg.Split('=')
+
+        return -join($parts[0],'=','"',$parts[1],'"')
+    }
+
+    return $arg
+}
+
 # Prints the message with the passed arguments
 Write-Host "Execution Started in CLI mode with the following arguments: $args"
 
+$cleanArgs = "$args"
+foreach ($arg in $args) {
+    $replace = (QuoteArgument -arg $arg)
+    $cleanArgs = $cleanArgs.Replace($arg,  $replace)
+}
+
 # Constructs the JMeter command and execute it
-$command = -join(".\jmeter ","$args")
+$command = -join(".\jmeter ",$cleanArgs)
 Invoke-Expression $command
 
 # Prints the execution completed message
